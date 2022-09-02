1ef5c8999f4eee9c29f2a35a29dc010119f83397
SUNZHIYUAN0102 <zhiyuansun703@gmail.com>
2022-09-02 17:06:55 +0700

    完成ListView基础列表，水平列表，图标组件

--- lib/main.dart
+++ lib/main.dart
@@ -25,44 +25,90 @@ class MyApp extends StatelessWidget {
 class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      // child: Container(
-      //   width: 300,
-      //   height: 300,
-      //   decoration: BoxDecoration(
-      //     color: Colors.yellow,
-      //     // borderRadius: BorderRadius.all(
-      //     //   Radius.circular(20),
-      //     // ),
-      //     borderRadius: BorderRadius.circular(150),
-      //     image: DecorationImage(
-      //       image: NetworkImage("https://picsum.photos/id/237/200/200"),
-      //       fit: BoxFit.cover,
-      //     ),
-      //   ),
-      //   // child: Image.network(
-      //   //   "https://picsum.photos/id/237/200/200",
-      //   //   alignment: Alignment.center,
-      //   //   // color: Colors.yellow,
-      //   //   // colorBlendMode: BlendMode.colorDodge,
-      //   //   // fit: BoxFit.cover,
-      //   //   // fit: BoxFit.contain,
-      //   //   // repeat: ImageRepeat.repeatX,
-      //   // ),
-      // ),
+    //padding: EdgeInsets.all(10),
+    // ignore: prefer_const_literals_to_create_immutables
 
-      child: Container(
-        width: 200,
-        height: 200,
-        child: ClipOval(
-          // child: Image.network(
-          //   "https://picsum.photos/id/237/200/200",
-          // ),
-          child: Image.asset(
-            "images/avatar.jpg",
-            fit: BoxFit.cover,
+    //垂直列表
+    // children: <Widget>[
+    //   ListTile(
+    //     leading: Icon(
+    //       Icons.settings,
+    //       color: Colors.yellow,
+    //       size: 40,
+    //     ),
+    //     title: Text(
+    //       "handsome little yuan yuan yuan",
+    //       style: TextStyle(fontSize: 18),
+    //     ),
+    //     subtitle: Text("yuan yuan yuan yuan yuan yuan yuan"),
+    //     trailing: Icon(Icons.abc),
+    //   ),
+    //   ListTile(
+    //     leading: Image.network("https://picsum.photos/id/237/200/200"),
+    //     title: Text("handsome little yuan yuan yuan"),
+    //     subtitle: Text("yuan yuan yuan yuan yuan yuan yuan"),
+    //     trailing: Image.network("https://picsum.photos/id/237/200/300"),
+    //   ),
+    //   ListTile(
+    //     leading: Icon(Icons.pages),
+    //     title: Text("handsome little yuan yuan yuan"),
+    //     subtitle: Text("yuan yuan yuan yuan yuan yuan yuan"),
+    //   )
+    // ],
+
+    //垂直图文列表
+    // children: <Widget>[
+    //   Image.network("https://picsum.photos/id/237/500/500"),
+    //   Container(
+    //     child: Text(
+    //       "Title",
+    //       textAlign: TextAlign.center,
+    //       style: TextStyle(
+    //         fontSize: 18,
+    //       ),
+    //     ),
+    //     height: 40,
+    //     padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+    //   ),
+    //   Image.network("https://picsum.photos/id/237/500/500"),
+    //   Image.network("https://picsum.photos/id/237/500/500"),
+    //   Image.network("https://picsum.photos/id/237/500/500"),
+    //   Image.network("https://picsum.photos/id/237/500/500"),
+    // ],
+
+    //水平列表
+    return Container(
+      height: 180,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          Container(
+            width: 180,
+            height: 180,
+            color: Colors.deepOrange,
           ),
-        ),
+          Container(
+            width: 180,
+            height: 180,
+            color: Colors.deepPurpleAccent,
+            child: ListView(
+              children: <Widget>[
+                Image.network("https://picsum.photos/id/237/100/100"),
+                Image.network("https://picsum.photos/id/237/100/100"),
+              ],
+            ),
+          ),
+          Container(
+            width: 180,
+            height: 180,
+            color: Colors.blue,
+          ),
+          Container(
+            width: 180,
+            height: 180,
+            color: Colors.pink,
+          )
+        ],
       ),
     );
   }
