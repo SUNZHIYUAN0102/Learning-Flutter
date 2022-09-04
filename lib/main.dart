f3942fc4841a157016e06bb9a392793eb8da14d7
SUNZHIYUAN0102 <zhiyuansun703@gmail.com>
2022-09-04 22:01:25 +0700

    完成wrap组件

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         ),
         body: LayoutDemo(),
       ),
-      theme: ThemeData(primarySwatch: Colors.yellow),
+      theme: ThemeData(primarySwatch: Colors.green),
     );
   }
 }
@@ -26,153 +26,45 @@ class MyApp extends StatelessWidget {
 class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // return Center(
-    //   child: Container(
-    //     width: 200,
-    //     child: AspectRatio(
-    //       aspectRatio: 2.0 / 1.0,
-    //       child: Container(
-    //         color: Colors.red,
-    //       ),
-    //     ),
-    //   ),
-    // );
+    return Container(
+      width: 400,
+      height: 600,
+      color: Colors.red,
+      child: Wrap(
+        direction: Axis.vertical,
+        spacing: 10,
+        runSpacing: 10,
+        alignment: WrapAlignment.center,
+        runAlignment: WrapAlignment.spaceBetween,
+        children: [
+          MyButton("第一季"),
+          MyButton("第一季, 第一季, 第一季"),
+          MyButton("第一季"),
+          MyButton("第一季"),
+          MyButton("第一季"),
+          MyButton("第一季"),
+          MyButton("第一季"),
+          MyButton("第一季"),
+          MyButton("第一季"),
+          MyButton("第一季"),
+          MyButton("第一季")
+        ],
+      ),
+    );
+  }
+}
 
-    // return AspectRatio(
-    //   aspectRatio: 2 / 1,
-    //   child: Container(
-    //     color: Colors.orange,
-    //   ),
-    // );
-
-    // return ListView(
-    //   children: [
-    //     Card(
-    //       margin: EdgeInsets.all(10),
-    //       child: Column(
-    //         children: [
-    //           ListTile(
-    //             title: Text(
-    //               "YuanYuan",
-    //               style: TextStyle(
-    //                 fontSize: 28,
-    //               ),
-    //             ),
-    //             subtitle: Text("Programmer"),
-    //           ),
-    //           ListTile(
-    //             title: Text("Phone: 89996193980"),
-    //             subtitle: Text("Address: Xxxxxxxx"),
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //     Card(
-    //       margin: EdgeInsets.all(10),
-    //       child: Column(
-    //         children: [
-    //           ListTile(
-    //             title: Text(
-    //               "YuanYuan2",
-    //               style: TextStyle(
-    //                 fontSize: 28,
-    //               ),
-    //             ),
-    //             subtitle: Text("Programmer"),
-    //           ),
-    //           ListTile(
-    //             title: Text("Phone: 89996193980"),
-    //             subtitle: Text("Address: Xxxxxxxx"),
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //     Card(
-    //       margin: EdgeInsets.all(10),
-    //       child: Column(
-    //         children: [
-    //           ListTile(
-    //             title: Text(
-    //               "YuanYuan3",
-    //               style: TextStyle(
-    //                 fontSize: 28,
-    //               ),
-    //             ),
-    //             subtitle: Text("Programmer"),
-    //           ),
-    //           ListTile(
-    //             title: Text("Phone: 89996193980"),
-    //             subtitle: Text("Address: Xxxxxxxx"),
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //   ],
-    // );
-
-    // return ListView(
-    //   children: [
-    //     Card(
-    //       margin: EdgeInsets.all(10),
-    //       child: Column(
-    //         children: [
-    //           AspectRatio(
-    //             aspectRatio: 16 / 9,
-    //             child: Image.network(
-    //               "https://picsum.photos/id/237/1600/900",
-    //               fit: BoxFit.cover,
-    //             ),
-    //           ),
-    //           ListTile(
-    //             // leading: ClipOval(
-    //             //   child: Image.network("https://picsum.photos/id/239/200/200"),
-    //             // ),
-    //             leading: CircleAvatar(
-    //               backgroundImage:
-    //                   NetworkImage("https://picsum.photos/id/239/200/200"),
-    //             ),
-    //             title: Text("YuanYuanYuan"),
-    //             subtitle: Text(
-    //               "This is long long long long long long long long long long long story",
-    //               style: TextStyle(overflow: TextOverflow.ellipsis),
-    //             ),
-    //           )
-    //         ],
-    //       ),
-    //     ),
-    //   ],
-    // );
-
-    return ListView(
-      children: listData.map((value) {
-        return Card(
-          margin: EdgeInsets.all(10),
-          child: Column(
-            children: [
-              AspectRatio(
-                aspectRatio: 16 / 9,
-                child: Image.network(
-                  value["imageUrl"],
-                  fit: BoxFit.cover,
-                ),
-              ),
-              ListTile(
-                // leading: ClipOval(
-                //   child: Image.network("https://picsum.photos/id/239/200/200"),
-                // ),
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(value["imageUrl"]),
-                ),
-                title: Text("YuanYuanYuan"),
-                subtitle: Text(
-                  "This is long long long long long long long long long long long story",
-                  style: TextStyle(overflow: TextOverflow.ellipsis),
-                ),
-              )
-            ],
-          ),
-        );
-      }).toList(),
+class MyButton extends StatelessWidget {
+  String text;
+  MyButton(this.text);
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.white),
+      ),
     );
   }
 }
